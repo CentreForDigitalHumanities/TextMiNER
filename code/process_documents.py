3fa28c532bf1704b237da48a8ee34bf2d9a06ca3
BeritJanssen <berit.janssen@gmail.com>
2024-07-03 17:18:00 +0200

    add filter fields, save source data, scroll

--- code/process_documents.py
+++ code/process_documents.py
@@ -1,7 +1,9 @@
+import click
 import logging
+from os.path import join
+import pickle
 import string
 
-import click
 from elasticsearch import BadRequestError
 from flair.splitter import SegtokSentenceSplitter
 from flair.models import SequenceTagger
@@ -12,6 +14,8 @@ import torch
 from es_client import es_client
 
 logger = logging.getLogger(__name__)
+
+SOURCE_DATA_DIR = './data'
 
 
 def dutch_tagger():
@@ -42,27 +46,46 @@ ner_models = {
 def process_documents(index, field_name, language):
     es = es_client()
     add_annotated_field(es, index, field_name)
-    documents = es.search(index=index, size=1000)['hits']['hits']
+    add_filter_fields(es, index)
+    initial_search = es.search(index=index, size=1000, scroll='1m')
+    if not initial_search:
+        es.clear_scroll(scroll_id='_all')
+    documents = initial_search['hits']['hits']
+    n_documents = len(documents)
+    scroll_id = initial_search['_scroll_id']
+    total_hits = initial_search['hits']['total']['value']
     tagger = ner_models.get(language)
+    annotate_entities(documents, field_name, tagger, es, index)
+    while n_documents < total_hits:
+        documents = es.scroll(scroll_id=scroll_id, scroll='60m')[
+            'hits']['hits']
+        annotate_entities(documents, field_name, tagger, es, index)
+
+
+def annotate_entities(documents, field_name, tagger, es_client, index):
     for doc in documents:
         output = ''
+        entities = []
         document = doc['_source'][field_name]
         splitter = SegtokSentenceSplitter()
         sentences = splitter.split(document)
         for sentence in sentences:
             try:
                 tagger.predict(sentence)
-                output = parse_prediction(sentence, output) + ' '
+                output = parse_prediction(sentence, output, entities) + ' '
             except:
                 logger.warning(
                     'Failed to parse the following sentence: {}'.format(sentence))
-        es.update(index=index, id=doc['_id'], doc={
-            annotated_field_name(field_name): output[:-1]
+        save_entity_labels(entities, index, doc['_id'])
+        es_client.update(index=index, id=doc['_id'], doc={
+            annotated_field_name(field_name): output[:-1],
+            **create_filter_content(entities)
         })
 
 
-def parse_prediction(sentence, output):
+def parse_prediction(sentence, output, entities):
     predicted = sentence.to_dict()
+    entities.extend(predicted['entities'])
     tokens = predicted['tokens']
     for pos, token in enumerate(tokens):
         whitespace = '' if (
@@ -81,6 +104,21 @@ def parse_prediction(sentence, output):
     return output
 
 
+def create_filter_content(entities) -> dict:
+    document_fields = {}
+    for ent in entities:
+        for field in filter_field_mappings().keys():
+            if field in [label['value'] for label in ent['labels']]:
+                document_fields.update(
+                    {filter_field_mappings()[field]: ent['text']})
+    return document_fields
+
+
+def save_entity_labels(entities, index_name, identifier):
+    with open(join(SOURCE_DATA_DIR, f'{index_name}.pkl'), 'rb') as f:
+        pickle.dump({identifier: entities}, f)
+
+
 def add_opening_tag(entity, whitespace):
     return whitespace + '[' + entity['text']
 
@@ -90,7 +128,7 @@ def add_closing_tag(entity):
 
 
 def annotated_field_name(field_name):
-    return '{}_ner'.format(field_name)
+    return '{}:ner'.format(field_name)
 
 
 def add_annotated_field(es_client, index_name, field_name):
@@ -102,6 +140,27 @@ def add_annotated_field(es_client, index_name, field_name):
                         'type': 'annotated_text'
                     }
                 }
+        )
+    except BadRequestError:
+        raise
+
+
+def filter_field_mappings():
+    return {
+        'PER': 'ner:person',
+        'LOC': 'ner:location',
+        'ORG': 'ner:organization',
+        'MISC': 'ner:miscellaneous'
+    }
+
+
+def add_filter_fields(es_client, index_name):
+    try:
+        es_client.indices.put_mapping(
+            index=index_name,
+            properties={field_name: {'type': 'keyword'}
+                        for field_name in filter_field_mappings().values()
+                        }
         )
     except BadRequestError:
         raise
